84160e031ddb9c1b85d04f6ed65b0ed95e906cf4
piresvrp <piresvrp@gmail.com>
2018-12-23 21:53:40 -0200

    alteracões de inconsistencias

--- cadastro.pl
+++ cadastro.pl
@@ -4,7 +4,7 @@ print <<"HTML";
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-<title>Diferenças</title>
+<title>Diferenï¿½as</title>
 <link rel="stylesheet" type="text/css" href="arquivo.css" />
 <link rel="shortcut icon" href="images/perl-logo80.jpg-777b35b3b943633b.jpeg" type="image/x-icon" /> 
 </head>
@@ -20,7 +20,7 @@ print <<"HTML";
 
 	<div id="titulo_tutorial">
 		<div id="login">
-        	<center><h3 style="color:white;">Casdastro de Usuário</h3></center>
+        	<center><h3 style="color:white;">Casdastro de UsuÃ¡rio</h3></center>
         </div>
 	  </form>
    
@@ -30,10 +30,10 @@ print <<"HTML";
 	<div id="menu">
 		
 		<ul>
-			<li><a href="index.pl" title="O que é perl">Perl</a></li>
-			<li><a href="instalacao.pl"title="Instalçao do perl" >Instalação</a></li>
+			<li><a href="index.pl" title="O que Ã© perl">Perl</a></li>
+			<li><a href="instalacao.pl"title="InstalÃ§Ã£o do perl" >Instalaï¿½ï¿½o</a></li>
 			<li><a href="linguagem.pl"title="Sobre a linguagem Perl">Linguagem e Sintaxe</a></li>
-			<li><a href="diferencas.pl"class="active"title="Diferença de Perl para Outras Linguagens">Diferenças de Outras Linguagens</a></li>
+			<li><a href="diferencas.pl"class="active"title="DiferenÃ§a de Perl para Outras Linguagens">Diferenï¿½as de Outras Linguagens</a></li>
             <li><a href="baixar.pl" title="Downloads">Downloads</a></li>
             <li><a href="sobre.pl"title="sobre desenvolvedores">Sobre</a></li>
 		</ul>
@@ -48,7 +48,7 @@ print <<"HTML";
             	
             <form name="contato" action="" method="post">
    		    <fieldset style="width:250px;height:190px"><br/>
-   			   <center><legend>Cadastro de Usuário </legend><br/>
+   			   <center><legend>Cadastro de Usuï¿½rio </legend><br/>
                
                 		<label>Nome:&nbsp;&nbsp; 
                   		<input type="String" name="nome" size="20"/></label><br /><br/>
@@ -73,7 +73,7 @@ print <<"HTML";
 
 	<div id="rodape">
 			<div class="left">&copy; 2015 All rights reserved.</div>
-			<div class="right">Desenvolvedor <a href="http://www.nodethirtythree.com/">Vinícius Resende</a></div>
+			<div class="right">Desenvolvedor <a href="http://www.nodethirtythree.com/">Vinï¿½cius Resende</a></div>
 	</div>
 	
 </div>
